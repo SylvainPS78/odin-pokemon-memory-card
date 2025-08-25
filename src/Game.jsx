8b249d17e401d3012b17ae9d71aad25d2f228a3d
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-25 11:08:13 +0700

    FEAT Create DifficultyModal + style

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,17 @@
 import "./assets/styles/Game.css";
 import MainScreen from "./assets/components/MainScreen.jsx";
 import Footer from "./assets/components/Footer.jsx";
+import DifficultyModal from "./assets/components/DifficultyModal.jsx";
+
+let isModalOpen = true;
+let isGameActive = false;
 
 function Game() {
   return (
     <>
-      <MainScreen />
+      {isModalOpen && <DifficultyModal />}
+      {isGameActive && <MainScreen />}
+
       <Footer />
     </>
   );
---
+++ src/assets/components/DifficultyModal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DifficultyModal = () => {
+  return (
+    <div className="difficulty-modal">
+      <h2 className="difficulty-modal-title">Select a difficulty level</h2>
+      <button className="difficulty-modal-button">Easy</button>
+      <button className="difficulty-modal-button">Medium</button>
+      <button className="difficulty-modal-button">Hard</button>
+    </div>
+  );
+};
+
+export default DifficultyModal;
