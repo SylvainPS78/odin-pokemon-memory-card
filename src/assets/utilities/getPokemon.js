5b39f1243ab532e807e67abf7a8099687d5f0b5d
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-08-26 08:45:32 +0700

    FEAT Load smaller .png on mobile devices

--- src/assets/utilities/getPokemon.js
+++ src/assets/utilities/getPokemon.js
@@ -1,9 +1,30 @@
+const isMobileDevice = () => {
+  return (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    ) || screen.width <= 768
+  );
+};
+
 const getPokemon = async (index) => {
   const id = index;
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const { name, sprites } = await res.json();
-  const image =
-    sprites.other.dream_world.front_default || sprites.front_default;
+
+  let image;
+
+  if (isMobileDevice()) {
+    image =
+      sprites.front_default ||
+      sprites.other.home.front_default ||
+      sprites.other.dream_world.front_default;
+  } else {
+    image =
+      sprites.other.dream_world.front_default ||
+      sprites.other.home.front_default ||
+      sprites.front_default;
+  }
+
   return { name, image, id };
 };
 
